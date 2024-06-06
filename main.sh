943408c200c3054f9ac06f8ce358958d2115fdb9
Precious Mozia <chibundumozia@gmail.com>
2024-06-06 18:19:10 +0200

    Added unique id generation

--- main.sh
+++ main.sh
@@ -2,14 +2,16 @@
 
 echo "Welcome to Xmen"
 echo "What do you want to do today? Please select an option"
-echo "Select an option (1-3):"
+echo "Select an option (1-5):"
 echo "1. Add a student"
 echo "2. List all students"
 echo "3. Delete student"
+echo "4. Update file"
 echo "5. Exit the application"
 read choice
 
 function option1 {
+    unique_id=$(uuidgen)
     echo "Hello, You have chosen option 1!"
     echo "Please enter the student's name"
     read name
@@ -19,8 +21,8 @@ function option1 {
     read intake
     echo "Please enter the student's email"
     read email
-    echo "$name, $age, $intake, $email" >> students-list_1023.txt
-    echo "Thank you, $name, $age, $intake, $email have been added to the record"
+    echo "$unique_id, $name, $age, $intake, $email" >> students-list_1023.txt
+    echo "Thank you, $name, $age, $intake, $email have been added to the record with unique ID $unique_id"
 }
 
 function option2 {
@@ -52,6 +54,14 @@ function option3 {
         echo "No record found for '$email'."
     fi
 }
+  
+function option4 {
+  echo "You selected option 4."
+  echo "enter the student you want to update"
+  grep -v "$email" students-list_1023.txt
+  sed 's/old_text/new_text/g' students-list_1023.txt
+  echo "You have updated the text"
+  }
 
 function option5 {
   echo "You selected option 5."
@@ -72,6 +82,9 @@ case $choice in
         ;;
     3)
         option3
+        ;;
+    4)
+        option4
         ;;
     5)
         option5

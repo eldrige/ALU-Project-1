41c6c5ccb15c9b6efa5c346d7245508c47f0d022
Eldrige <peldrige8@gmail.com>
2024-06-06 15:02:23 +0200

    add script to add username to list

--- main.sh
+++ main.sh
@@ -1,18 +1,45 @@
 #!/bin/bash
 
 echo "Welcome to Xmen: "
-echo "What do you want to do today?"
-echo "Please select an option"
+echo "What do you want to do today? Please select an option"
+echo "Select an option (1-3):"
 read choice
-echo "Hello, You have choosen,  $choice!"
+
+function option1 {
+  echo "Hello, You have choosen, $choice!"
+  echo "Please enter the students name"
+  read name
+  echo "Please enter the students age"
+  read age
+  echo "Please enter the students intake"
+  read intake
+  echo "$name, $age, $intake" >> students-list_1023.txt
+  echo "Thank you, Your record had been added"
+}
+
+function option2 {
+  echo "You selected option 2."
+}
+
+function option3 {
+  echo "You selected option 3."
+}
+
+function default_option {
+  echo "Invalid option selected...Please start again"
+}
 
 case $choice in
   1)
-    echo "You want to add a new record." ;;
+    option1
+    ;;
   2)
-    echo "You want to list all students." ;;
+    option2
+    ;;
   3)
-    echo "You want to delete a student." ;;
+    option3
+    ;;
   *)
-    echo "Invalid input. Please try again" ;;
+    default_option
+    ;;
 esac

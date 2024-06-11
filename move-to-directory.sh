7378869f3630e166f68c8edfc14c6e56d34c19eb
SibahleD <169036979+SibahleD@users.noreply.github.com>
2024-06-11 22:06:21 +0200

    Readme.md additions

--- move-to-directory.sh
+++ move-to-directory.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-mkdir testing
+# This creates the directory
+mkdir negpod_1-q1
 
-cp ./* ./testing
+# Files are moved from the current directory to the new folder
+mv ./* ./negpod_1-q1
 
-if find ./testing -mindepth 1 -maxdepth 1 | read 
+# A message is printed when the folder is not empty
+if find ./negpod_1-q1 -mindepth 1 -maxdepth 1 | read 
 then
 	echo "Successful!"
 else 
 	echo "Unsuccessful"
 fi
-
-

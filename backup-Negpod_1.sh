72a53e9b950f1af6b3e4a902e6571be0830bf2e6
Eldrige <peldrige8@gmail.com>
2024-06-11 17:51:42 +0200

    update file for transfer

--- backup-Negpod_1.sh
+++ backup-Negpod_1.sh
@@ -1,43 +1,19 @@
-# #!/bin/bash
-
-# # Set the necessary variables
-# REMOTE_HOST="ec1ebc754f75.a5efec7a.alu-cod.online"
-# REMOTE_USER="ec1ebc754f75"
-# REMOTE_BACKUP_DIR="/path/to/remote/backup-Negpod_1.sh"
-# LOCAL_BACKUP_DIR="/summative/0524-2024m"
-# BACKUP_FILE="negpod_1-q1-$(date +%Y-%m-%d).tar.gz"
-
-# # Create the local backup
-# echo "Creating local backup..."
-# tar -czf "$LOCAL_BACKUP_DIR/$BACKUP_FILE" /path/to/files/to/backup
-
-# # Upload the backup to the remote server
-# echo "Uploading backup to remote server..."
-# scp "$LOCAL_BACKUP_DIR/$BACKUP_FILE" "$REMOTE_USER@$REMOTE_HOST:$REMOTE_BACKUP_DIR/$BACKUP_FILE"
-
-# # Clean up the local backup
-# echo "Cleaning up local backup..."
-# rm "$LOCAL_BACKUP_DIR/$BACKUP_FILE"
-
-# echo "Backup complete!"
-
-
 #!/bin/bash
 
-# Set the necessary variables
-REMOTE_HOST="ec1ebc754f75.a5efec7a.alu-cod.online"
-REMOTE_USER="ec1ebc754f75"
-REMOTE_PASS="ea1d8f222b11d7be18f6"
+# Set the variables
+REMOTE_HOST="7d7ba2c0ed04.70681577.alu-cod.online"
+REMOTE_USER="7d7ba2c0ed04"
+REMOTE_PASS="db8ccf55ac830058265d"
 REMOTE_DIR="/summative/0524-2024m"
-LOCAL_DIR="student_files"
-BACKUP_FILE="backup-negpod_1.tar.gz"
+LOCAL_DIR="./negpod_1-q1/"
 
 # Create the backup archive
-tar -czf $BACKUP_FILE $LOCAL_DIR
+tar -czf backup.tar.gz $LOCAL_DIR
 
 # Transfer the backup to the remote server
-sshpass -p "$REMOTE_PASS" scp $BACKUP_FILE $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR/
+sshpass -p "$REMOTE_PASS" scp backup.tar.gz "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR"
 
-echo "Backup complete."
+# Clean up the local backup
+rm backup.tar.gz
 
-
+echo "Backup completed successfully!"
